0ea8cf2aac21af86eb50a41321e0bda26094208d
EGOIST <0x142857@gmail.com>
2022-08-25 15:39:03 +0800

    add `author` field to default example

--- src/lib/examples.ts
+++ src/lib/examples.ts
@@ -29,7 +29,7 @@ export const examples: {
 					<div class="text-zinc-400 mt-3 flex items-center space-x-2">
 						<div class="flex items-center space-x-1">
 							<img class="w-5 h-5 rounded-full" :src="avatar" />
-							<span>The Onion</span>
+							<span>{{author}}</span>
 						</div>
 						<span>-</span>
 						<span>April 4</span>
@@ -41,6 +41,7 @@ export const examples: {
 			title: 'Teachers Reveal Why They Are Quitting Their Jobs',
 			description:
 				'Amid budget cuts, restrictive curricula, and increasing threats to their safety, more and more teachers are opting to switch careers. The Onion asked teachers to explain why they are quitting their jobs, and this is what they said.',
+			author: 'The Onion',
 			avatar: 'https://cdn.jsdelivr.net/gh/egoist-bot/images@main/upic/nIib90.png',
 			bg: 'https://cdn.jsdelivr.net/gh/egoist-bot/images@main/upic/U5tMDX.png'
 		}
